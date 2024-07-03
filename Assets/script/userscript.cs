5f7377680a68e59e7d8696269fb318c5234ac0fd
고혁빈 <rhgurqls00@gmail.com>
2024-07-04 02:02:59 +0900

    스크립트 수정 / 스프라이트 저장, 위치 지정

---
+++ Assets/script/userscript.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class userscript : MonoBehaviour
+{
+    public GameObject control;
+    public GameObject movepiece;
+
+    private int xposition = -1;
+    private int yposition = -1;
+
+    private string player;
+
+    public Sprite whiteking, whitequeen, whiteknight, whitebishop, whiterook, whitepawn;
+    public Sprite blackking, blackqueen, blackknight, blackbishop, blackrook, blackpawn;
+
+    public void activate()
+    {
+        control = GameObject.FindGameObjectWithTag("GameController");
+
+        SetCoords();
+
+        switch (this.name)
+        {
+            case "blackqueen": this.GetComponent<SpriteRenderer>().sprite = blackqueen; break;
+            case "blackking": this.GetComponent<SpriteRenderer>().sprite = blackking; break;
+            case "blackrook": this.GetComponent<SpriteRenderer>().sprite = blackrook; break;
+            case "blackknight": this.GetComponent<SpriteRenderer>().sprite = blackknight; break;
+            case "blackbishop": this.GetComponent<SpriteRenderer>().sprite = blackbishop; break;
+            case "blackpawn": this.GetComponent<SpriteRenderer>().sprite = blackpawn; break;
+
+            case "whitequeen": this.GetComponent<SpriteRenderer>().sprite = whitequeen; break;
+            case "whiteking": this.GetComponent<SpriteRenderer>().sprite = whiteking; break;
+            case "whiterook": this.GetComponent<SpriteRenderer>().sprite = whiterook; break;
+            case "whiteknight": this.GetComponent<SpriteRenderer>().sprite = whiteknight; break;
+            case "whitebishop": this.GetComponent<SpriteRenderer>().sprite = whitebishop; break;
+            case "whitepawn": this.GetComponent<SpriteRenderer>().sprite = whitepawn; break;
+        }
+    }
+
+    public void SetCoords()
+    {
+        float x = xposition;
+        float y = yposition;
+
+        x *= 0.66f;
+        y *= 0.66f;
+
+        x += -2.3f;
+        y += -2.3f;
+
+        this.transform.position = new Vector3(x,y,-1.0f);
+    }
+}
